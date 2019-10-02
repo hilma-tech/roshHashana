60554bfb600321775ede37c998635559fe794cfb
erangep <eran.gep@gmail.com>
2019-10-02 14:25:53 +0300

    updated package.json

--- scripts/start.js
+++ scripts/start.js
@@ -126,8 +126,8 @@ checkBrowsers(paths.appPath, isInteractive)
         console.log();
       }
 
-      console.log(chalk.cyan('Starting the development server...\n'));
-      openBrowser(urls.localUrlForBrowser);
+      console.log(chalk.cyan('Starting server at port'+port+'\n'));
+     // openBrowser(urls.localUrlForBrowser);
     });
 
     ['SIGINT', 'SIGTERM'].forEach(function(sig) {
