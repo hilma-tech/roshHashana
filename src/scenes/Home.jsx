f75fc2039863dd6a2e4f77df8fc0177880b514bb
erangep <eran.gep@gmail.com>
2019-10-06 14:41:23 +0300

    updates pumba title

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
       <header className="App-header">
         <img src="favicon.ico" className="App-logo" alt="logo" />
         <p>
-          Welcome to Carmel 6000's Professional Updated Management App 
+          Welcome to Carmel 6000's Proffesional Updated Main Boilerplate App
         </p>
         <a className="App-link" href="/admin" target="_blank" rel="noopener noreferrer">Access Dashboard</a>
       </header>
