25bb46642b748249196565ca4813806f7ad7c824
reut avital <reutavital@carmel6000.amitnet.org>
2020-07-19 12:21:50 +0300

    add create public meeting function and call this function from InsertDataShofarBlower

--- common/models/shofarBlower.js
+++ common/models/shofarBlower.js
@@ -1,63 +1,83 @@
 'use strict';
 
-module.exports = function(ShofarBlower) {
+module.exports = function (ShofarBlower) {
     //role = 2
 
-// {
-// "can_blow_x_times":1,
-//             "volunteering_start_time":1594563291554,
-//             "volunteering_end_time":1594563291554,
-//         "city" : "חיפה",
-//         "street": "פרויד",
-//         "appartment": "23",
-//         "comments": null
-//     }
-   ShofarBlower.InsertDataShofarBlower = async (data,options) => {
-       
-       
-       let objToBlower = {
-            "userBlowerId":options.accessToken.userId,
-            "can_blow_x_times":data.can_blow_x_times,
-            "volunteering_start_time":data.volunteering_start_time,
-            "volunteering_end_time":data.volunteering_end_time
+    // {
+    // "can_blow_x_times":1,
+    //             "volunteering_start_time":1594563291554,
+    //             "volunteering_end_time":1594563291554,
+    //         "city" : "חיפה",
+    //         "street": "פרויד",
+    //         "appartment": "23",
+    //         "comments": null
+    //     }
+    ShofarBlower.InsertDataShofarBlower = async (data, options) => {
+        let city;
+        try {
+            //check if the city is exist in city table
+            city = await ShofarBlower.app.models.City.findOne({ where: { "name": data.city } });
+            //the city doesnt exist-> create the city
+            if (!city.id) {
+                try {
+                    city = await ShofarBlower.app.models.City.addNewCity(data.city, options);
+                }
+                catch (err) {
+                    throw err;
+                }
+            }
+        } catch (error) {
+            throw error;
+        }
+
+
+        let objToBlower = {
+            "userBlowerId": options.accessToken.userId,
+            "can_blow_x_times": data.can_blow_x_times,
+            "volunteering_start_time": data.volunteering_start_time,
+            "volunteering_end_time": data.volunteering_end_time
         },
-        objToCU={
-            "city" : data.city,
-            "street": data.street,
-            "appartment": data.appartment,
-            "comments": data.comments    
-        };
-       
-       
-       try {
-           let resRole = await ShofarBlower.app.models.RoleMapping.findOne({where:{principalId : options.accessToken.userId }});
-           if (resRole.roleId === 2){
+            objToCU = {
+                "city": city.id,
+                "street": data.street,
+                "appartment": data.appartment,
+                "comments": null
+            };
+
+
+        try {
+            let resRole = await ShofarBlower.app.models.RoleMapping.findOne({ where: { principalId: options.accessToken.userId } });
+            if (resRole.roleId === 2) {
                 let resBlower = await ShofarBlower.create(objToBlower)
-                let resCU = await ShofarBlower.app.models.CustomUser.updateAll({id : options.accessToken.userId }, objToCU);
-                return {ok : true};
-           }else{
-               return {ok : false, err:"No permissions"};
-           }
-           
-
-       } catch (error) {
-        console.log("Can`t do create new isolated",error); 
-       throw error;
-       }
-
-   }
+                let resCU = await ShofarBlower.app.models.CustomUser.updateAll({ id: options.accessToken.userId }, objToCU);
+                //if the shofar blower added publicPlaces, create them
+                if (data.publicPlaces) {
+                    let resPublicMeetings = await ShofarBlower.app.models.shofarBlowerPub.createNewPubMeeting(data.publicPlaces, options.accessToken.userId, options);
+                }
+                return { ok: true };
+            } else {
+                return { ok: false, err: "No permissions" };
+            }
 
 
+        } catch (error) {
+            console.log("Can`t do create new isolated", error);
+            throw error;
+        }
+
+    }
 
 
-ShofarBlower.remoteMethod('InsertDataShofarBlower', {
-    http: { verb: 'post' },
-    accepts: [
-      { arg: 'data', type: 'object' },
-      { arg: 'options', type: 'object', http: "optionsFromRequest" }
-    ],
-    returns: { arg: 'res', type: 'object', root: true }
-});
+
+
+    ShofarBlower.remoteMethod('InsertDataShofarBlower', {
+        http: { verb: 'post' },
+        accepts: [
+            { arg: 'data', type: 'object' },
+            { arg: 'options', type: 'object', http: "optionsFromRequest" }
+        ],
+        returns: { arg: 'res', type: 'object', root: true }
+    });
 
 
 }
--- common/models/shofarBlowerPub.js
+++ common/models/shofarBlowerPub.js
@@ -1,4 +1,67 @@
 'use strict';
 
-module.exports = function(shofarBlowerPub) {}
+module.exports = function (shofarBlowerPub) {
+
+    //data -> [{
+    //     "city": "ירושלים",
+    //     "street": "גדעון",
+    //     "comments":"ליד הבית ספר",
+    //     "start_time": 2020-07-19 08:05:47,
+    //     "blowerId": 4
+    // },]
+
+    shofarBlowerPub.createNewPubMeeting = async (data, blowerId, options) => {
+        for (let i = 0; i < data.length; i++) {
+            let meetingData = data[i];
+            if (options.accessToken && options.accessToken.userId) {
+                console.log(meetingData)
+
+                //find the city or create one
+                let city;
+                try {
+                    //check if the city is exist in city table
+                    city = await shofarBlowerPub.app.models.City.findOne({ where: { "name": meetingData.city } });
+                    //the city doesnt exist-> create the city
+                    if (!city.id) {
+                        try {
+                            city = await shofarBlowerPub.app.models.City.addNewCity(meetingData.city, options);
+                        }
+                        catch (err) {
+                            throw err;
+                        }
+                    }
+                } catch (error) {
+                    throw error;
+                }
+
+                let newPubMeeting = {
+                    "cityId": city.id,
+                    "street": meetingData.street,
+                    "comments": meetingData.placeDescription,
+                    "start_time": meetingData.time,
+                    "blowerId": blowerId
+                }
+
+                try {
+                    //create new public meeting
+                    let res = await shofarBlowerPub.create(newPubMeeting);
+                    console.log(res, 'res');
+                    if (res) return res.id;
+
+                } catch (error) {
+                    throw error;
+                }
+            }
+        }
+    }
+
+    shofarBlowerPub.remoteMethod('createNewPubMeeting', {
+        http: { verb: 'post' },
+        accepts: [
+            { arg: 'data', type: 'object' },
+            { arg: 'options', type: 'object', http: 'optionsFromRequest' },
+        ],
+        returns: { arg: 'res', type: 'object', root: true }
+    });
+}
 
