e4ad37fcf4181739e7b5ac82318d0c6d7e490c72
reut avital <reutavital@carmel6000.amitnet.org>
2020-08-09 16:22:51 +0300

    add draggable front end

--- src/components/sb_route_list.jsx
+++ src/components/sb_route_list.jsx
@@ -1,16 +1,18 @@
-import React, { useContext } from 'react';
-import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+import React, { useContext, useState, useEffect } from 'react';
 import moment from 'moment'
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 
 import { SBContext } from '../ctx/shofar_blower_context';
+import { changePosition } from '../fetch_and_utils';
 
 const SBRouteList = (props) => {
-    const sbCtxVal = useContext(SBContext)
-    const { userData, totalTime, totalLength } = sbCtxVal;
-    if (!userData) return null;
-    let myMeetings = Array.isArray(sbCtxVal.myMeetings) ? [...sbCtxVal.myMeetings] : [];
+    const { userData, totalTime, totalLength, myMeetings: myRoute, setMyMeetings: setMyRoute } = useContext(SBContext);
+    const [startPoint, setStartPoint] = useState({});
+    useEffect(() => {
+        if (!userData) return null;
+        setStartPoint(userData)
 
-    const myRoute = [userData, ...myMeetings]
+    }, []);
 
     const textStart = "משך הליכה כולל"
     const msTT = totalTime
@@ -33,6 +35,48 @@ const SBRouteList = (props) => {
     const textValue = `${tt} ${timeUnits} ${length ? `(${length} ${lengthUnits})` : ""}`
     // textValue = `35 דקות (3 ק"מ)`//testing   
 
+    const SortableItem = SortableElement(({ value }) =>
+        createItemContent(value.value, value.index)
+    );
+
+    const SortableList = SortableContainer(({ items }) => {
+        return (
+            <ul>
+                {items.map((value, index) => (
+                    < SortableItem key={`item-${index}`} index={index} value={{ value: value, index: index }} />
+                ))}
+            </ul>
+        );
+    });
+
+    const createItemContent = (value, index) => {
+        return (<div key={"sb-route-list-" + index} className="meeting-in-route" >
+            <div className="meeting-in-route-img-container" >
+                <div className="meeting-in-route-img">
+                    {index === -1 ?
+                        <img src="/icons/white_shofar.svg" />
+                        : index + 1}
+                </div>
+            </div>
+            <div className="meeting-in-route-info-container">
+                <div className="meeting-in-route-info-1">
+                    <div className="meeting-in-route-title" >{index === -1 ? "נקודת יציאה" : (value.isPublicMeeting ? "קריאה ציבורית" : value.name)}</div>
+                    <div className="meeting-in-route-location" >{typeof value.address === "string" ? value.address.split(", ישראל").join('') : ""}</div>
+                    <div className="meeting-in-route-comments" >{value.comments || ""}</div>
+                </div>
+                <div className="meeting-in-route-info-2">
+                    <img src={value.isPublicMeeting ? "/icons/group-orange.svg" : "/icons/single-blue.svg"} alt={value.isPublicMeeting ? "תקיעה ציבורית" : "תקיעה פרטית"} />
+                    <div className="meeting-in-route-time">{moment(value.startTime).format("HH:mm")}</div>
+                </div>
+            </div>
+        </div>)
+    }
+
+    const onSortEnd = ({ oldIndex, newIndex }) => {
+        setMyRoute(
+            changePosition(myRoute, oldIndex, newIndex),
+        );
+    };
     return (
         <div className="sb-route-list" >
             <div className="sb-side-list-title" >
@@ -42,30 +86,8 @@ const SBRouteList = (props) => {
                 {`${textStart}: ${textValue}`}
             </div>
             <div className="sb-list">
-                {myRoute.map((m, i) => {
-                    return (
-                        <div key={"sb-route-list-" + i} className="meeting-in-route" >
-                            <div className="meeting-in-route-img-container" >
-                                <div className="meeting-in-route-img">
-                                    {i == 0 ?
-                                        <img src="/icons/white_shofar.svg" />
-                                        : i}
-                                </div>
-                            </div>
-                            <div className="meeting-in-route-info-container">
-                                <div className="meeting-in-route-info-1">
-                                    <div className="meeting-in-route-title" >{i == 0 ? "נקודת יציאה" : (m.isPublicMeeting ? "קריאה ציבורית" : m.name)}</div>
-                                    <div className="meeting-in-route-location" >{typeof m.address === "string" ? m.address.split(", ישראל").join('') : ""}</div>
-                                    <div className="meeting-in-route-comments" >{m.comments || ""}</div>
-                                </div>
-                                <div className="meeting-in-route-info-2">
-                                    <img src={m.isPublicMeeting ? "/icons/group-orange.svg" : "/icons/single-blue.svg"} alt={m.isPublicMeeting ? "תקיעה ציבורית" : "תקיעה פרטית"} />
-                                    <div className="meeting-in-route-time">{moment(m.startTime).format("HH:mm")}</div>
-                                </div>
-                            </div>
-                        </div>
-                    );
-                })}
+                {startPoint && createItemContent(startPoint, -1)}
+                <SortableList lockToContainerEdges={true} lockAxis={'y'} items={myRoute} onSortEnd={onSortEnd} />
             </div>
         </div>
     );
--- src/fetch_and_utils.js
+++ src/fetch_and_utils.js
@@ -85,3 +85,9 @@ export const viewportToPixels = value => {
     var side = window[['innerHeight', 'innerWidth'][['vh', 'vw'].indexOf(parts[2])]]
     return side * (q / 100)
 }
+
+export const changePosition = (array, from, to) => {
+    array = array.slice();
+    array.splice(to < 0 ? array.length + to : to, 0, array.splice(from, 1)[0]);
+    return array;
+}
