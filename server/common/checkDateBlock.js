e25d74b54e261a09f7fcf87e21c343be91118468
reut avital <reutavital@carmel6000.amitnet.org>
2020-08-19 17:18:38 +0300

    fix date to block

--- server/common/checkDateBlock.js
+++ server/common/checkDateBlock.js
@@ -1,7 +1,7 @@
 const CONSTS = require('./consts/consts');
 
 module.exports = function checkDateBlock() {
-    let now = new Date(Date.now());
+    let now = new Date().toLocaleString("en-US", { timeZone: "Asia/Jerusalem" });
     if (Date.parse(CONSTS.DATE_TO_BLOCK) < Date.parse(now)) {
         //CONSTS.DATE_TO_BLOCK is less than now
         //block
