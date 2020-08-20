6f15347b940c21d70e68494cf3b89ae5eaed962a
shanike <shani@carmel6000.amitnet.org>
2020-08-20 12:00:39 +0300

    css on sb_assign_meeting to browser / mobile

--- src/components/sb_assign_meeting.jsx
+++ src/components/sb_assign_meeting.jsx
@@ -249,12 +249,12 @@ const SBAssignMeeting = ({ history, inRoute }) => {
     return (
         <div className={`${isBrowser ? "sb-assign-container" : "sb-assign-mobile-container"} ${openAssign ? "open-animation" : "close-animation"}`} id="sb-assign-container" >
 
-            <div id="assign-x-btn-cont" >
+            <div id="assign-x-btn-cont" style={{ margin: isBrowser ? "6% 0" : "3% 0" }} >
                 <img src="/icons/close.svg" id="assign-x-btn" onClick={() => { handleAssignment("close") }} />
             </div>
 
-            <div className="assign-title-container">
-                <div id="assign-title" className="width100" >{inRoute ? 'אלו הם פרטי מפגש תקיעת שופר' : 'שיבוץ תקיעה בשופר'}</div>
+            <div className="assign-title-container" style={{ marginBottom: isBrowser ? "6%" : "3%", marginTop: isBrowser ? "3%" : "3%" }} >
+                <div id="assign-title" style={{ marginBottom: isBrowser ? "10%" : "5%" }} className="width100" >{inRoute ? 'אלו הם פרטי מפגש תקיעת שופר' : 'שיבוץ תקיעה בשופר'}</div>
 
                 <div id="assign-icon-and-text-cont" className="width100" >
                     <img id="assign-icon" src={iconSrc} />
@@ -263,7 +263,7 @@ const SBAssignMeeting = ({ history, inRoute }) => {
                 {inRoute && assignMeetingInfo.isPublicMeeting ? <div id="signedCount">{assignMeetingInfo.signedCount ? assignMeetingInfo.signedCount === 1 ? `רשום אחד לתקיעה` : `${assignMeetingInfo.signedCount} רשומים לתקיעה` : "טרם קיימים רשומים לתקיעה"}</div> : null}
             </div>
 
-            <div className="sb-assign-content-container">
+            <div style={{ margin: isBrowser ? "20% 0" : "2% 0" }} className="sb-assign-content-container">
                 <div className="inputDiv" id="meeting-name" >{assignMeetingInfo.isPublicMeeting ? "תקיעה ציבורית" : assignMeetingInfo.name}</div>
                 {assignMeetingInfo.isPublicMeeting ? null : < div className={`inputDiv ${!assignMeetingInfo.phone ? 'no-value-text' : ''}`} id="meeting-phone" >{assignMeetingInfo.phone ? assignMeetingInfo.phone : 'אין מספר פלאפון להציג'}</div>}
                 <div className="inputDiv" id="meeting-address" >{assignMeetingInfo.address}</div>
@@ -272,7 +272,7 @@ const SBAssignMeeting = ({ history, inRoute }) => {
                 {inRoute && walkDuration ? <div className="walk-duration" >{`זמן הליכה מהנקודה הקודמת ${walkDuration}`}</div> : null}
             </div>
 
-            {block ? null : (inRoute ? <div className="delete-meeting clickAble" onClick={deleteMeeting}>הסירו את מפגש התקיעה מהמסלול שלי ומהמאגר</div> : <button id="assign-btn" onClick={() => { handleAssignment() }} >שבץ אותי</button>)}
+            {block ? null : (inRoute ? <div className="delete-meeting clickAble" style={{ marginTop: isBrowser ? "10%" : "5%" }} onClick={deleteMeeting}>הסירו את מפגש התקיעה מהמסלול שלי ומהמאגר</div> : <button id="assign-btn" style={{ marginTop: isBrowser ? "10%" : "5%" }} onClick={() => { handleAssignment() }} >שבץ אותי</button>)}
         </div>
     );
 }
