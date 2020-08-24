0deea95bf0995cccaf210d9b50045ec2b7b179a8
reut avital <reutavital@carmel6000.amitnet.org>
2020-08-24 15:05:18 +0300

    fix style of register and home page

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -31,12 +31,12 @@ const Home = (props) => {
                 </div>
                 <div className="content-container" >
 
-                    <img alt="" style={{ width: isBrowser ? '21vw' : '67vw' }} src="/images/header.svg" onLoad={updateNumImgLoaded} />
+                    <img alt="" style={{ width: isBrowser ? '21vw' : '55vw' }} src="/images/header.svg" onLoad={updateNumImgLoaded} />
                     <div className="d-lg-none d-md-none text-light " style={{ fontSize: "150%", fontWeight: "bold" }} >
                         <div className="stuckHouse">{stuckHouse}</div>
                     </div>
                     <div className="buttonAll justify-content-center align-items-center">
-                        <div className={`${isBrowser ? "browserButtonForRegister" : "mobileButtonForRegister"} ` + ' row justify-content-center '} style={{ width: isBrowser && '56vw', marginTop: isBrowser && "3%", margin: !isBrowser && "10% auto 0% auto", }}>
+                        <div className={`${isBrowser ? "browserButtonForRegister" : "mobileButtonForRegister"} ` + ' row justify-content-center '} style={{ width: isBrowser && '56vw', marginTop: isBrowser && "3%", margin: !isBrowser && "0% auto", }}>
                             <button style={{ marginBottom: !isBrowser && '5%' }} className={`${isBrowser ? "browserRegisterIsolator" : "mobileRegisterIsolator"}`} value="isolator" onClick={(e) => onClickRegister(e.target.value)}>
                                 אני רוצה לשמוע תקיעת שופר  </button>
                             <button className={`${isBrowser ? "browserRegisterBlower" : "mobileRegisterBlower"}`} value="blower" onClick={(e) => onClickRegister(e.target.value)}>
@@ -48,7 +48,7 @@ const Home = (props) => {
                         <div className="text-light clickAble" id="text1" onClick={() => setOpenMap(true)}>מפת תקיעות ארצית</div>
                     </>
                         : <>
-                            <div onClick={() => setOpenMap(true)} className="text-light" style={{ fontSize: "3vh", marginTop: "12%", whiteSpace: "pre-line", lineHeight: "1.2" }}>{allMeeting}</div>
+                            <div onClick={() => setOpenMap(true)} className="text-light" style={{ fontSize: "3vh", marginTop: "5%", whiteSpace: "pre-line", lineHeight: "1.2" }}>{allMeeting}</div>
                             <div onClick={() => setOpenMap(true)} className="img-container"><img alt="" style={{ width: '12vw' }} src="/images/map.svg" onLoad={updateNumImgLoaded} /></div></>
                     }
                     <div id="shivuki-credit">{'תוכן שיווקי: אפרת שפירא www.my-idea.co.il'}</div>
