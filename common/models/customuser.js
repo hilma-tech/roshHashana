9d018b9aea8e8063ee4ba6d9f5f9a3cac7de6598
shanike <shani@carmel6000.amitnet.org>
2020-08-25 14:19:57 +0300

    user id 10 to var of uesrId

--- common/models/customuser.js
+++ common/models/customuser.js
@@ -550,7 +550,7 @@ module.exports = function (CustomUser) {
                 let [errPublicMeeting, resPublicMeeting] = await executeMySqlQuery(CustomUser,
                     `select count(isolated.id) as participantsNum , shofar_blower_pub.id as meetingId, blowerId as userId
                          from isolated right join shofar_blower_pub on  shofar_blower_pub.id = isolated.blowerMeetingId 
-                         where (blowerId = 10) 
+                         where (blowerId = ${userId}) 
                          group by shofar_blower_pub.id `);
                 if (resPublicMeeting && Array.isArray(resPublicMeeting)) {
                     let meetingsToUpdate = [], meetingsToDelete = [];
