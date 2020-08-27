ec2821bbb2bed846f9aeea4a81b8216a1635dad7
reut avital <reutavital@carmel6000.amitnet.org>
2020-08-27 16:46:10 +0300

    script to send sms to all users without data (address)

---
+++ please_add_data_script.js
@@ -0,0 +1,34 @@
+const { sendMsg } = require('./server/sendSms/SendSms');
+let mysql = require('mysql');
+const query = `SELECT CustomUser.name , CustomUser.username AS 'phoneNumber', RoleMapping.roleId 
+             FROM CustomUser 
+                LEFT JOIN RoleMapping ON RoleMapping.principalId = CustomUser.id 
+             WHERE RoleMapping.roleId=1 OR RoleMapping.roleId =2;
+`
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "z10mz10m",
+    database: "roshHashana"
+});
+
+let msg = '';
+con.connect(err => {
+    if (err) throw err
+    con.query(query, (err, users, _fields) => {
+        if (err) throw err;
+        for (let user of users) {
+            if (user.roleId == 2) {// the user is shofar blower
+                msg = `שלום, \nנראה כי לא סיימת את הרשמתך כבעל תוקע. \nלהמשך הרשמה אנא הכנס לקישור https://shofar2all.com/ והשלם את הפרטים החסרים.\nלפניות ובקשות נוספות, ניתן ליצור קשר במייל ב shofar2all@gmail.com.\nתודה, ושנה טובה צוות יום תרועה`;
+                sendMsg(user.phoneNumber, msg)
+            } else if (user.roleId == 1) {
+                msg = `שלום,\nראינו באתר שנרשמת על מנת שיתקעו עבורך בשופר. אך נראה כי לא סיימת את הרשמתך ולכן לא נוכל לסייע לך.\nלהמשך הרשמה אנא הכנס לקישור https://shofar2all.com והשלם את הפרטים החסרים. \nלפניות ובקשות נוספות, ניתן ליצור קשר במייל ב shofar2all@gmail.com. \nתודה, ושנה טובה צוות יום תרועה`;
+                sendMsg(user.phoneNumber, msg)
+            }
+            else {
+                msg = '';
+                // return;
+            }
+        }
+    });
+});
