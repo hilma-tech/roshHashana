e40e7f4938eb3db942a6f3596a0cf682eadb13ae
reshitAriel <reshariel@gmail.com>
2020-08-30 10:17:58 +0300

    admin - add shofar blower

--- common/models/shofarBlower.js
+++ common/models/shofarBlower.js
@@ -280,4 +280,61 @@ module.exports = function (ShofarBlower) {
         returns: { arg: 'res', type: 'object', root: true }
     });
 
+    ShofarBlower.InsertDataShofarBlowerAdmin = async (data, options) => {
+        try {
+            const userId = data.userId
+            delete data.userIs
+            let blowerInfo = await ShofarBlower.findOne({ where: { "userBlowerId": userId } });
+            if (!blowerInfo) {
+                if (!Array.isArray(data.address) || data.address.length !== 2) { console.log("ADDRESS NOT VALID"); return { ok: false, err: "כתובת אינה תקינה" } }
+                if (!data.address[0] || data.address[0] === "NOT_A_VALID_ADDRESS" || typeof data.address[1] !== "object" || !data.address[1].lng || !data.address[1].lat) { console.log("ADDRESS NOT VALID"); return { ok: false, err: 'נא לבחור מיקום מהרשימה הנפתחת' } }
+                data.address[0] = data.address[0].substring(0, 398) // shouldn't be more than 400 
+                if (data.address && data.address[0]) {
+                    let addressArr = data.address[0]
+                    if (typeof addressArr === "string" && addressArr.length) {
+                        addressArr = addressArr.split(", ")
+                        let city = ShofarBlower.app.models.CustomUser.getLastItemThatIsNotIsrael(addressArr, addressArr.length - 1);
+                        data.city = city || addressArr[addressArr.length - 1];
+                    }
+                }
+                let objToBlower = {
+                    "userBlowerId": userId,
+                    "can_blow_x_times": data.can_blow_x_times,
+                    "volunteering_start_time": data.volunteering_start_time,
+                    "volunteering_max_time": data.volunteering_max_time
+                },
+                    objToCU = {
+                        "address": data.address[0],
+                        "city": data.city,
+                        "lng": data.address[1].lng,
+                        "lat": data.address[1].lat,
+                        "comments": null
+                    };
+
+                let resRole = await ShofarBlower.app.models.RoleMapping.findOne({ where: { principalId: userId } });
+
+                let resBlower = await ShofarBlower.create(objToBlower)
+                let resCU = await ShofarBlower.app.models.CustomUser.updateAll({ id: userId }, objToCU);
+                //if the shofar blower added publicPlaces,
+                if (data.publicPlaces) {
+                    let [errPublicMeetings, resPublicMeetings] = await to(ShofarBlower.app.models.shofarBlowerPub.createNewPubMeeting(data.publicPlaces, userId, options, true));
+                    if (errPublicMeetings) { console.log("errPublicMeetings", errPublicMeetings); return { ok: false } }
+                    if (resPublicMeetings) { console.log("resPublicMeetings", resPublicMeetings); }
+                }
+                return { ok: true };
+            }
+        } catch (error) {
+            console.log("Can`t create new isolated", error);
+            throw error;
+        }
+    }
+
+    ShofarBlower.remoteMethod('InsertDataShofarBlowerAdmin', {
+        http: { verb: 'post' },
+        accepts: [
+            { arg: 'data', type: 'object' },
+            { arg: 'options', type: 'object', http: "optionsFromRequest" }
+        ],
+        returns: { arg: 'res', type: 'object', root: true }
+    });
 }
