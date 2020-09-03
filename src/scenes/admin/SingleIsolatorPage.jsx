c1d6861c063e325ccb0c1eebc665456eddcc3812
maayanLCohen <maayan45633@gmail.com>
2020-09-03 10:54:24 +0300

    next prev page admin

--- src/scenes/admin/SingleIsolatorPage.jsx
+++ src/scenes/admin/SingleIsolatorPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AdminMainContext } from './ctx/AdminMainContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import AdminMap from '../../components/maps/adminMap'
 
 const SingleIsolatorPage = (props) => {
     const { selectedIsolator, setSelectedIsolator } = useContext(AdminMainContext)
@@ -28,6 +29,12 @@ const SingleIsolatorPage = (props) => {
                 <div id="meeting-type" >{`מפגש ${selectedIsolator && selectedIsolator.isPublicMeeting ? "תקיעה ציבורי" : "תקיעה פרטי"}`}</div>
             </div>
 
+            <AdminMap
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&language=he&key=${process.env.REACT_APP_GOOGLE_KEY_SECOND}`}
+                loadingElement={<img alt="נטען..." className="loader" src='/images/loader.svg' />}
+                containerElement={<div style={{ height: `100%` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+            />
         </div>
     );
 }
