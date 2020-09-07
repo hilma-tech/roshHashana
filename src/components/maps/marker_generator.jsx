593be861b97559626de4f19abd89971585eb2f0f
reut avital <reutavital@carmel6000.amitnet.org>
2020-09-07 13:07:33 +0300

    fix problem marker info window

--- src/components/maps/marker_generator.jsx
+++ src/components/maps/marker_generator.jsx
@@ -20,7 +20,7 @@ const MarkerGenerator = (props) => {
     }
 
     let url = (type === PRIVATE_MEETING) ? (props.isolated || props.blower) ? '/icons/single.svg' : '/icons/single-blue.svg' : (props.isolated || props.blower) ? '/icons/group.svg' : '/icons/group-orange.svg';
-
+    console.log(isInfoWindowOpen, 'isInfoWindowOpen', info, 'info')
     const icon = props.icon ? props.icon : {
         url: url,
         scaledSize: (type === PRIVATE_MEETING) ? ((props.isolated || props.blower) ? new window.google.maps.Size(50, 50) : new window.google.maps.Size(30, 30)) : new window.google.maps.Size(50, 50), // the svg borders and margins משפיעים here
@@ -42,7 +42,7 @@ export default MarkerGenerator;
 
 
 
-export const SBMarkerGenerator = ({ iconUrl: iconUrlProps, location, info, markerIcon, iconType, defaultInfoState = false, onClick }) => {
+export const SBMarkerGenerator = ({ iconUrl: iconUrlProps, location, info, markerIcon, iconType, defaultInfoState = false }) => {
     /**
      * icon: overrides props.type
      * location
@@ -67,13 +67,13 @@ export const SBMarkerGenerator = ({ iconUrl: iconUrlProps, location, info, marke
         origin: new window.google.maps.Point(0, 0),
         anchor: new window.google.maps.Point(15, 15), // changes position of icon
     }
+    console.log(isInfoWindowOpen, 'isInfoWindowOpen', info, 'info')
 
     return (
         <Marker
             icon={icon}
             onClick={closeOrOpenInfoWindow}
             position={new window.google.maps.LatLng(latNum, lngNum)}
-            onClick={onClick || undefined}
         >
             {info && isInfoWindowOpen ? <InfoWindow onCloseClick={closeOrOpenInfoWindow}>{info}</InfoWindow> : null}
         </Marker>
