915d0728724427b1bff53797c04b1d3361fc9369
reut avital <reutavital@carmel6000.amitnet.org>
2020-09-13 17:37:47 +0300

    send sms when blower is conirmed and update block dates in consts

--- src/consts/const_messages.js
+++ src/consts/const_messages.js
@@ -26,6 +26,6 @@ export const CONSTS = {
             { featureType: "transit", stylers: [{ visibility: "off" }] }]
     },
     JERUSALEM_POSITION: { lat: 31.771959, lng: 35.217018 },
-    DATE_TO_BLOCK_ISOLATED: new Date(2020, 8, 20, 23, 59, 0).toLocaleString("en-US", { timeZone: "Asia/Jerusalem" }), // 20/9/2020 23:58
-    DATE_TO_BLOCK_BLOWER: new Date(2020, 8, 19, 23, 59, 0).toLocaleString("en-US", { timeZone: "Asia/Jerusalem" }) // 19/9/2020 23:58
+    DATE_TO_BLOCK_ISOLATED: new Date(2020, 8, 16, 23, 59, 0).toLocaleString("en-US", { timeZone: "Asia/Jerusalem" }), // 16/9/2020 23:58
+    DATE_TO_BLOCK_BLOWER: new Date(2020, 8, 16, 23, 59, 0).toLocaleString("en-US", { timeZone: "Asia/Jerusalem" }) // 16/9/2020 23:58
 }
