bc19ce5d283f0a59deb8a5d74a5614780ca07d37
reut avital <reutavital@carmel6000.amitnet.org>
2020-09-17 16:54:06 +0300

    fix block date

--- server/common/consts/consts.js
+++ server/common/consts/consts.js
@@ -2,5 +2,5 @@ module.exports = {
     SHOFAR_BLOWING_DURATION_MS: 300000, // 5 min
     CURRENTLY_BLOCKED_ERR: 'BLOCKED',
     DATE_TO_BLOCK_ISOLATED: new Date(2020, 8, 17, 10, 0, 0).toLocaleString("en-US", { timeZone: "Asia/Jerusalem" }), // 17/9/2020 10:00
-    DATE_TO_BLOCK_BLOWER: new Date(2020, 8, 17, 20, 0, 0).toLocaleString("en-US", { timeZone: "Asia/Jerusalem" }) // 17/9/2020 20:00
+    DATE_TO_BLOCK_BLOWER: new Date(2020, 8, 17, 16, 53, 0).toLocaleString("en-US", { timeZone: "Asia/Jerusalem" }) // 17/9/2020 20:00
 }
