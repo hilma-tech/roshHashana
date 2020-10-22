8426c6857801d28939e98087578009adf0155076
Naama Shiponi <naamas1234567@gmail.com>
2020-10-22 08:54:30 +0300

    off sms

--- server/sendSms/SendSms.js
+++ server/sendSms/SendSms.js
@@ -5,18 +5,18 @@ const sendMsg = async (phoneNum, msg) => {
     const data = `<?xml version="1.0" encoding="UTF-8"?><sms><user><username>Fb9KF2fX</username><password>RX70n5eE</password></user><source>Shofar2all</source><destinations><phone>${phoneNum}</phone></destinations><message>${messageText}</message><response>0</response></sms>`;
     const options = { hostname: 'www.019sms.co.il', port: 443, path: '/api', method: 'POST', headers: { 'Content-Type': 'application/json', 'Content-Length': data.length } };
 
-    const req = https.request(options, res => {
+    // const req = https.request(options, res => {
 
-        res.on('data', d => {
-            // process.stdout.write(d)
-        });
-    })
+    //     res.on('data', d => {
+    //         // process.stdout.write(d)
+    //     });
+    // })
 
-    req.on('error', error => {
-    });
+    // req.on('error', error => {
+    // });
 
-    req.write(data);
-    // console.log("req", req);
-    req.end();
+    // req.write(data);
+    // // console.log("req", req);
+    // req.end();
 }
 module.exports = { sendMsg };
